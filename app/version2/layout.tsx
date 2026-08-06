220153e3ff6927ce3b6ca6e64ddb3fe99342ad37
mrreduck-lab <mrreduck@gmail.com>
2026-08-06 21:22:32 +0300

    Load version2 interaction fixes

--- app/version2/layout.tsx
+++ app/version2/layout.tsx
@@ -1,5 +1,6 @@
 import './v2.css';
 import './v2-upgrade.css';
+import './v2-fixes.css';
 
 export default function Version2Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
